429bbc28aef866ae68433da03c131c5c10ac541f
Jens <ojdomela@gmail.com>
2022-07-26 21:44:20 +0200

    added storybook-static

---
+++ storybook-static/assets/es.map.constructor.69298dfc.js
@@ -0,0 +1,2 @@
+import{n as o,o as t}from"./iframe.04f4eb71.js";var r=o,e=t;r("Map",function(n){return function(){return n(this,arguments.length?arguments[0]:void 0)}},e);
+//# sourceMappingURL=es.map.constructor.69298dfc.js.map
