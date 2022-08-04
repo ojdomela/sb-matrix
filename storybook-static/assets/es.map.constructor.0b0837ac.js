f8c1d070f910a45aeb1fb53cab3f2bbe5deee29b
Jens <ojdomela@gmail.com>
2022-08-04 22:07:54 +0200

    updated sb build

---
+++ storybook-static/assets/es.map.constructor.0b0837ac.js
@@ -0,0 +1,2 @@
+import{n as o,o as t}from"./iframe.1382d9d7.js";var r=o,e=t;r("Map",function(n){return function(){return n(this,arguments.length?arguments[0]:void 0)}},e);
+//# sourceMappingURL=es.map.constructor.0b0837ac.js.map
